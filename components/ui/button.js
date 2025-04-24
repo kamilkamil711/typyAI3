ffc5b89e3c084e772ecdaf018f15a2ac7b900518
kamilkamil711 <kamil.padewski1@gmail.com>
2025-04-24 11:58:38 +0200

    Delete components/ui directory

--- components/ui/button.js
+++
@@ -1,3 +0,0 @@
-export function Button({ children }) {
-  return <button className="bg-black text-white px-4 py-2 rounded-xl">{children}</button>;
-}
--- components/ui/input.js
+++
@@ -1,3 +0,0 @@
-export function Input(props) {
-  return <input className="border p-2 rounded w-full" {...props} />;
-}
